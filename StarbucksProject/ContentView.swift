88eec08f177539552b638f140ea8c2b29db19e53
Im-loki <al4126@naver.com>
2023-11-02 15:36:18 +0900

    콘텐트뷰 설정

--- StarbucksProject/ContentView.swift
+++ StarbucksProject/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+   @StateObject var beverageStore = BeverageStore(beverages: beverageData)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            ForEach (0..<beverageStore.beverages.count, id: \.self) { i in
+                ListCell(beverage: beverageStore.beverages[i])
+            }
         }
-        .padding()
     }
 }
 
@@ -29,6 +29,65 @@ struct Beverage: Codable, Identifiable {
     var imageName: String
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
+// JSON 파일을 로드하는 표준 방식, 이후 앱에서도 사용이 가능
+func loadJson<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("\(filename) not found.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Could not load \(filename): \(error)")
+    }
+
+    do {
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        fatalError("Unable to parse \(filename): \(error)")
+    }
+
+}
+
+var beverageData: [Beverage] = loadJson("starbucksData.json")
+
+class BeverageStore : ObservableObject {
+
+    @Published var beverages: [Beverage]
+
+    init(beverages: [Beverage] = []) {
+        self.beverages = beverages
+    }
+}
+
+// 사용자 인테페이스 콘텐트 뷰 설계
+
+
+// 하위 뷰로 추출
+struct ListCell: View {
+
+    var beverage: Beverage
+
+    var body: some View {
+        HStack {
+            Image(beverage.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100, height: 60)
+            Text(beverage.name)
+        }
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
