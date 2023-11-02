bb3b3a387e8b6d8e4c52b1cf6485a2ecdf9457a0
Eunice <jes.e.0927@gmail.com>
2023-11-02 16:56:32 +0900

    Feat: 메뉴 추가 뷰 생성
    Content View의 좌측 상단 Add 버튼을 누르면 메뉴 추가 뷰로 연결
    BeverageAddView에서 하단의 Add 버튼을 누르면 Content View로 복귀

---
+++ StarbucksProject/View/BeverageAddView.swift
@@ -0,0 +1,84 @@
+//
+//  BeverageAddView.swift
+//  StarbucksProject
+//
+//  Created by Eunsu JEONG on 11/2/23.
+//
+
+import SwiftUI
+
+struct BeverageAddView: View {
+    @StateObject var beverageStore: BeverageStore
+    @Binding var path: NavigationPath
+    
+    @State private var isAllergicMilk = false
+    @State private var isAllergicSoyBean = false
+    @State private var isAllergicWheat = false
+    @State private var name: String = ""
+    @State private var description: String = ""
+    
+    var body: some View {
+        Form {
+            Section {
+                Image(systemName: "takeoutbag.and.cup.and.straw")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                DataInput(title: "메뉴", userInput: $name)
+                
+                DataInput(title: "설명", userInput: $description)
+            } header: { Text("음료 설명") }
+            
+            Section {
+                Toggle(isOn: $isAllergicMilk) {
+                    Text("우유")
+                }
+                Toggle(isOn: $isAllergicWheat) {
+                    Text("밀")
+                }
+                Toggle(isOn: $isAllergicSoyBean) {
+                    Text("대두")
+                }
+                
+            } header: { Text("알레르기 유발요인") }
+        }
+        Button {
+            addNewBeverage()
+        } label: { Text("새 음료 추가") }
+    }
+
+    func addNewBeverage() {
+        let newBeverage = Beverage(
+            id: UUID().uuidString,
+            name: name,
+            description: description,
+            isAllergicMilk: isAllergicMilk,
+            isAllergicSoyBean: isAllergicSoyBean,
+            isAllergicWheat: isAllergicWheat,
+            imageName: "tesla_model_3")
+        
+        beverageStore.beverages.append(newBeverage)
+        path.removeLast()
+    }
+}
+
+struct DataInput: View {
+    var title: String
+    @Binding var userInput: String
+    
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.headline)
+            
+            TextField("Enter \(title)", text: $userInput)
+                .textFieldStyle(.roundedBorder)
+        }
+    }
+}
+
+
+//#Preview {
+//    BeverageAddView(beverageStore: BeverageStore(beverages: beverageData))
+//}
--- StarbucksProject/View/ContentView.swift
+++ StarbucksProject/View/ContentView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 // 사용자 인테페이스 콘텐트 뷰 설계
 struct ContentView: View {
     @StateObject var beverageStore = BeverageStore(beverages: beverageData)
+    @State private var stackPath = NavigationPath() //네비게이션 경로 추가
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $stackPath) {
             List {
                 ForEach (0..<beverageStore.beverages.count, id: \.self) { index in
                     NavigationLink(value: index) {
@@ -22,13 +23,18 @@ struct ContentView: View {
             .navigationDestination(for: Int.self) { index in
                 BeverageDetailView(selectedBeverage: beverageStore.beverages[index])
             }
+            .navigationDestination(for: String.self, destination: { i in
+                BeverageAddView(beverageStore: self.beverageStore, path: $stackPath)
+            })
             .navigationTitle("스타벅스")
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     EditButton()
                 }
                 ToolbarItem(placement: .topBarLeading) {
-                    Button("Add") { }
+                    NavigationLink(value: "새 음료 추가") {
+                        Text("Add")
+                    }
                 }
             }
         }
